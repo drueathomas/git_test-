01f2a0e972fbda428786940c184b89b7945f6672
fdw101 <felicia.ogarro@gmail.com>
2015-08-06 19:34:47 -0400

    Initial Commit

---
+++ Final_git/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Final_git
+//
+//  Created by Felicia O'Garro on 8/6/15.
+//  Copyright (c) 2015 ComputerLab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
